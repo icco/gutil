95a55956a3143e7934c2e98f37262b3f71ba2647
Nat Welch <nat@natwelch.com>
2021-04-03 01:47:12 +0000

    fix bugs

--- logging/middleware.go
+++ logging/middleware.go
@@ -10,10 +10,12 @@ import (
 	"github.com/go-chi/chi/middleware"
 	"github.com/icco/zapdriver"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-// Middleware is a middleware for writing request logs in a stuctured
-// format to stackdriver.
+// Middleware is a middleware for writing request logs in a stuctured format to
+// stackdriver. It uses a chi Chain to make sure all of the deps are properly
+// included (RequestID and Recoverer).
 func Middleware(log *zap.Logger, projectID string) func(next http.Handler) http.Handler {
 	return chi.Chain(
 		middleware.RequestID,
@@ -22,34 +24,39 @@ func Middleware(log *zap.Logger, projectID string) func(next http.Handler) http.
 	).Handler
 }
 
-func Handler(logger *zap.Logger) func(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		var requestID string
-		if reqID := r.Context().Value(middleware.RequestIDKey); reqID != nil {
-			requestID = reqID.(string)
-		}
+// Handler does the actual work of the http middleware.
+func Handler(logger *zap.Logger, projectID string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			var requestID string
+			if reqID := r.Context().Value(middleware.RequestIDKey); reqID != nil {
+				requestID = reqID.(string)
+			}
 
-		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
-		next.ServeHTTP(ww, r)
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			next.ServeHTTP(ww, r)
+			latency := time.Since(start)
 
-		payload := zapdriver.NewHTTP(r, ww)
-		payload.Status = ww.Status()
-		payload.Latency = fmt.Sprintf("%.9fs", latency.Seconds())
+			payload := zapdriver.NewHTTP(r, nil)
+			payload.Status = ww.Status()
+			payload.Latency = fmt.Sprintf("%.9fs", latency.Seconds())
+			payload.ResponseSize = fmt.Sprintf("%d", ww.BytesWritten())
 
-		trace, span, sampled := ParseTraceHeader(r.Header.Get("X-Cloud-Trace-Context"))
-		if trace != "" {
-			fields = append(fields, zapdriver.TraceContext(trace, span, sampled, projectID)...)
-		}
-		fields = append(fields, zapdriver.HTTP(payload))
+			var fields []zapcore.Field
+			trace, span, sampled := ParseTraceHeader(r.Header.Get("X-Cloud-Trace-Context"))
+			if trace != "" {
+				fields = append(fields, zapdriver.TraceContext(trace, span, sampled, projectID)...)
+			}
+			fields = append(fields, zapdriver.HTTP(payload))
 
-		latency := time.Since(start)
-		if requestID != "" {
-			fields = append(fields, zap.String("request-id", requestID))
-		}
+			if requestID != "" {
+				fields = append(fields, zap.String("request-id", requestID))
+			}
 
-		logger.Info("request completed", fields...)
-	})
+			logger.Info("request completed", fields...)
+		})
+	}
 }
 
 // ParseTraceHeader takes a GCP trace header and translates it to a trace,
