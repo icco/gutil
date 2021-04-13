75e073c8d8474084fa99131e68b27b883a281a4a
Nat Welch <nat@natwelch.com>
2021-04-13 11:59:58 +0000

    Add metrics middleware

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -3,12 +3,16 @@ package metrics
 import (
 	"context"
 	"fmt"
+	"net/http"
 
 	mexporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/metric"
 	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/sdk/metric/controller/basic"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/semconv"
 	"go.uber.org/zap"
 )
 
@@ -22,11 +26,11 @@ func (eh *errHandler) Handle(err error) {
 }
 
 // Init sets up a default trace and metric provider.
-func Init(ctx context.Context, log *zap.SugaredLogger, projectID string) error {
-	if err := TraceInit(ctx, log, projectID); err != nil {
+func Init(ctx context.Context, log *zap.SugaredLogger, projectID, serviceName string) error {
+	if err := TraceInit(ctx, log, projectID, serviceName); err != nil {
 		return err
 	}
-	if err := MetricInit(ctx, log, projectID); err != nil {
+	if err := MetricInit(ctx, log, projectID, serviceName); err != nil {
 		return err
 	}
 
@@ -34,7 +38,7 @@ func Init(ctx context.Context, log *zap.SugaredLogger, projectID string) error {
 }
 
 // TraceInit sets up a default tracer for open telemetry.
-func TraceInit(ctx context.Context, log *zap.SugaredLogger, projectID string) error {
+func TraceInit(ctx context.Context, log *zap.SugaredLogger, projectID, serviceName string) error {
 	opts := []texporter.Option{
 		texporter.WithProjectID(projectID),
 		texporter.WithContext(ctx),
@@ -42,7 +46,10 @@ func TraceInit(ctx context.Context, log *zap.SugaredLogger, projectID string) er
 			log.Errorw("stackdriver trace error", zap.Error(err))
 		}),
 	}
-	topts := []trace.Option{}
+	topts := []trace.Option{
+		trace.WithSampler(sdktrace.AlwaysSample()),
+		trace.WithResource(resource.NewWithAttributes(semconv.ServiceNameKey.String(serviceName))),
+	}
 	tp, err := texporter.NewExportPipeline(opts, topts...)
 	if err != nil {
 		return fmt.Errorf("trace exporter init: %w", err)
@@ -56,7 +63,7 @@ func TraceInit(ctx context.Context, log *zap.SugaredLogger, projectID string) er
 }
 
 // MetricInit sets up a default metric provider for open telemetry.
-func MetricInit(ctx context.Context, log *zap.SugaredLogger, projectID string) error {
+func MetricInit(ctx context.Context, log *zap.SugaredLogger, projectID, serviceName string) error {
 	opts := []mexporter.Option{
 		mexporter.WithProjectID(projectID),
 		mexporter.WithContext(ctx),
@@ -64,7 +71,9 @@ func MetricInit(ctx context.Context, log *zap.SugaredLogger, projectID string) e
 			log.Errorw("stackdriver metric error", zap.Error(err))
 		}),
 	}
-	popts := []basic.Option{}
+	popts := []basic.Option{
+		basic.WithResource(resource.NewWithAttributes(semconv.ServiceNameKey.String(serviceName))),
+	}
 
 	pusher, err := mexporter.InstallNewPipeline(opts, popts...)
 	if err != nil {
@@ -72,4 +81,9 @@ func MetricInit(ctx context.Context, log *zap.SugaredLogger, projectID string) e
 	}
 
 	return nil
+}
+
+// Create http middleware.
+func Middleware(next http.HandlerFunc) http.Handler {
+	return otelhttp.NewHandler(next, "http")
 }
