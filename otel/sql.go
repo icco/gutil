88f48885347a566a722840ff77533c05ef2dcd63
Nat Welch <nat@natwelch.com>
2022-01-14 03:24:08 +0000

    fixes

--- otel/sql.go
+++ otel/sql.go
@@ -1,18 +1,17 @@
 package otel
 
 import (
-	"database/sql"
 	"fmt"
 
 	"github.com/XSAM/otelsql"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
-func InitPostgres() (*sql.Driver, error) {
+func InitPostgres() (string, error) {
 	// Register an OTel driver
-	driverName, err := otelsql.Register("postgres", semconv.DBSystemPostgreSQL.Value)
+	driverName, err := otelsql.Register("postgres", semconv.DBSystemPostgreSQL.Value.AsString())
 	if err != nil {
-		return nil, fmt.Errorf("could not create postgres otel: %w", err)
+		return "", fmt.Errorf("could not create postgres otel: %w", err)
 	}
 
 	return driverName, nil
