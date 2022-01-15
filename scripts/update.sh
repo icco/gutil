f3f3494424663029d5c8a9c5f174eab20e450e1b
Nat Welch <nat@natwelch.com>
2022-01-15 16:37:35 +0000

    update all

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
+set -ex
 
 SHA=$(git one)
 for i in $(cat ./repos.txt); do
   echo $i
+  tmp_dir=$(mktemp -d -t $i)
+  gh repo clone $i $tmp_dir
+  cd $tmp_dir
+  go get -v -u -d github.com/icco/gutil@$SHA
+  git diff
+  git ci -a -m 'update gutil'
+  go get -v -u -d  ./...
+  go mod tidy
+  go build -v ./...
+  git ci -a -m 'update'
+  git push -u
 done
